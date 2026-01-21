9c033fa09eda417de2b63cd5ea7337b6d7fbcf2f
Sandhya Verma <sandhyapatel0134@gmail.com>
2026-01-21 23:44:29 +0530

    Add files via upload

--- Find_Nth_root_of_a_number.cpp
+++ Find_Nth_root_of_a_number.cpp
@@ -1,33 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-int func(int mid, int N, int M){
-  long long  ans=1; 
-  for(int i=1; i<=N; i++){
-    ans*=mid;
-    if(ans>M)return 2;
-  }
-  if(ans==M)return 1;
-
-  return 0;
-
+    long long pow(long long base, int exp, int M) {
+        long long ans = 1;
+        while (exp > 0) {
+            if (exp & 1) {
+                ans *= base;
+                if (ans > M) return M + 1; // overflow control
+                exp--;
+            } else {
+                base *= base;
+                if (base > M) return M + 1;
+                exp /= 2;
+            }
+        }
+        return ans;
+    
 }
   int NthRoot(int N, int M) {
-        
-      int low=1, high=M;
-      while(low<=high){
-
-       int mid=(low+high)/2;
-        int midN =func(mid, N, M );
-if(midN==1){
-  return mid;
-}
-        else if(midN==0){
-          low=mid+1;
-        }
-        else {
-            high=mid-1;
-        }
-      }
-      return -1;
+       int low=0, high=M; 
+       while(low<=high){
+        int mid= low+(high-low)/2;
+       long long val= 1LL*pow(mid, N, M);
+       if(val==M)return mid;
+      else if(val<=M)low= mid+1;
+       else
+       high= mid-1;
+       
+       }
+       return -1;
     }
-  
+    
+    int main(){
+        int N, M;
+        cin>>N>>M;
+
+        
+        int ans= NthRoot(N, M);
+        cout<<ans<<endl;
+    }
