357e1df89f2296613cb2bce59caffccfd3fdb862
Sandhya Verma <sandhyapatel0134@gmail.com>
2026-01-21 22:40:55 +0530

    Add files via upload

---
+++ Find_out_how_many_times_the_array_is_rotated.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+int findKRotation(vector<int> &arr)  {
+        
+      int low=0, high=arr.size()-1;
+      int ans=INT_MAX;
+      int index=-1;
+      while(low<=high){
+        int mid=(low+high)/2;
+
+        
+        if(arr[low]<=arr[high]){
+          if(arr[low]<ans){
+            index=low;
+            ans=arr[low];
+          }
+          break;
+        }
+          if(arr[low]<=arr[mid]){
+          if(arr[low]<ans){
+            index=low;
+            ans=arr[low];
+          }
+          low=mid+1;
+          }
+          else{
+            if(arr[mid]< ans){
+              index=mid;
+              ans=arr[mid];
+            
+          }
+            high=mid-1;
+          }
+        }
+        return index;
+
+        
+     
+    }
+    
+
+    int main(){
+        int n; 
+        cin>>n;
+        
+        vector<int> arr(n);
+        for(int i=0; i<n; i++){
+            cin>>arr[i];
+        }
+        int ans= findKRotation(arr);
+        cout<<ans<<endl;
+    }
